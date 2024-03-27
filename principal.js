6a2aa4c63c0aac7a13d6f41498008b9c838fbbda
thaissa123456 <148073084+thaissa123456@users.noreply.github.com>
2024-03-27 09:13:09 -0300

    Update principal.js
    m

--- principal.js
+++ principal.js
@@ -10,3 +10,14 @@ for(let i=0;i <botoes.length;i++){
         botoes[i].classList.add("ativo");
     }
 }
+contadores [o].text content=calculatempo(tempo objetivo);
+function calcula tempo(tempo objetivo){
+    let tempoatual = new date();
+    let tempofinal=tempoobjetivo-tempoatual;
+    let segundos=math.floor(tempofinal\1000);
+    let minutos=math.floor(segundo\60)
+    let horas=math.floor(minutos\60);
+    let dias = math.floor(horas\24);
+    segundos%=60;
+    minutos%=60;
+    horas%=24;
