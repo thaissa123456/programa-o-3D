df494e6c3ba55271f2338c9206fc346a179942a5
thaissa123456 <148073084+thaissa123456@users.noreply.github.com>
2024-04-03 09:34:17 -0300

    .

--- principal.js
+++ principal.js
@@ -10,14 +10,27 @@ for(let i=0;i <botoes.length;i++){
         botoes[i].classList.add("ativo");
     }
 }
-contadores [o].text content=calculatempo(tempo objetivo);
-function calcula tempo(tempo objetivo){
-    let tempoatual = new date();
-    let tempofinal=tempoobjetivo-tempoatual;
-    let segundos=math.floor(tempofinal\1000);
-    let minutos=math.floor(segundo\60)
-    let horas=math.floor(minutos\60);
-    let dias = math.floor(horas\24);
-    segundos%=60;
-    minutos%=60;
-    horas%=24;
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2025-02-20T00:00:00");
+const tempoObjetivo2= new Date("2031-12-07T00:00:00");
+const tempoObjetivo3 = new Date("2024-04-05T00:00:00");
+const tempoObjetivo4 = new Date("2024-12-10T00:00:00");
+
+canst tempos =[tempopbjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+contadores [0]text content=calculatempo(tempos[0]);
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+ 
+ return dias + " dias " + horas + " horas " + min
