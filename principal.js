87beca890989e1f2e5e767cf155405af3ccff1f5
thaissa123456 <148073084+thaissa123456@users.noreply.github.com>
2024-07-31 10:16:50 -0300

    Update principal.js

--- principal.js
+++ principal.js
@@ -1,64 +1,22 @@
-const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba-conteudo");
+const caixaPrincipal = document querySelector(" caixa-principal") 
+; const caixaPerguntas = document. querySelector("•caixa-perguntas");   
+const caixaAlternativas = document. querySelector(" caixa-alternativas"); 
+const caixaResultado = document querySelector (" caixa-resultado"); 
+const textoResultado = document. querySelector (" texto-resultado");
 
-for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function () {
+const perguntas = [
 
-        for (let j = 0; j < botoes.length; j++) {
-            botoes[j].classList.remove("ativo");
-            textos[j].classList.remove("ativo");
-        }
+    enunciado: "Coloque o enunciado da pergunta 1", 
+    alternativas: [ 
+    {
+texto: "Primeira alternativa para essa pergunta", 
+afirmação: "afirmação que explica essa o motivo de escolher essa opção"
+}, 
 
-        botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
-    }
+texto: "Segunda alternativa para essa pergunta", 
+afirmção: "afirmação que explica essa o motivo de escolher essa opção"
 }
-
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
-
-const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
-
-
-function calculaTempo(tempoObjetivo) {
-    let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal / 1000);
-    let minutos = Math.floor(segundos / 60);
-    let horas = Math.floor(minutos / 60);
-    let dias = Math.floor(horas / 24);
-
-    segundos %= 60;
-    minutos %= 60;
-    horas %= 24;
-    if (tempoFinal > 0){
-        return [dias,horas,minutos,segundos];
-    } else {
-        return [0,0,0,0];
-    }
-}
-
-function atualizaCronometro(){
-    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
-    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
-    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
-    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
-
-    for (let i=0; i<contadores.length;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]); apagar essa linha
-        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
-        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
-        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
-        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
-    }
-}
-
-function comecaCronometro(){
-    atualizaCronometro();
-    setInterval(atualizaCronometro,1000);
-}
-
-comecaCronometro();
+]
+},
+{
+    enunciado:"
